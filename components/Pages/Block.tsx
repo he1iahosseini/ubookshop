58dc83628e619f4162f6913cd6cafd9a8e6be3e0
he1iahosseini <$heliahosseini0@gmail.com>
2024-09-09 16:28:28 +0430

    Update

---
+++ components/Pages/Block.tsx
@@ -0,0 +1,45 @@
+export const Block = props => {
+
+    if (!props.state.cart) {
+        props.state.cart = []
+      }
+
+    return <c-c style={{
+        width: 150, flex: 1, minWidth: 150,
+        position: "relative", backgroundColor: "white", borderRadius: 5, margin: "10px",
+        boxShadow: "0px 0px 5px 2px rgba(0,0,0,0.43)"
+    }}>
+
+        <img
+            className={global.styles.hover}
+            src={props.book.imageLink}
+            style={{
+                width: "100%", height: 200, objectFit: "fill", minWidth: 150, borderTopLeftRadius: 5,
+                borderTopRightRadius: 5
+            }}
+            onClick={() => {
+                props.state.form = "bookspecs"
+                props.state.book = props.book
+                props.refresh()
+            }} />
+
+        <f-cc style={{ padding: "5px 8px", width: "100%", height: 40, direction: "ltr" }}>
+            <f-12>{props.book.title}</f-12>
+        </f-cc>
+        <f-csb style={{ width: "100%", padding: "5px 0" }}>
+            <img src={props.state.cart.includes(props.book.title)?
+            "https://irmapserver.ir/research/37/check.png":
+            "https://irmapserver.ir/research/37/shopping-cart.png"}
+                style={{ height: 30, width: 30, objectFit: "contain", margin: "5px 5px" }} />
+
+
+            <c-x style={{ direction: "ltr", margin: "0 10px" }}>
+                <f-12><del>{(props.book.price as number).toLocaleString("fa-IR")} تومان</del></f-12>
+                <f-15>{(props.book.price* 0.8 as number).toLocaleString("fa-IR")} تومان</f-15>
+
+            </c-x>
+        </f-csb>
+
+        
+    </c-c>
+}
